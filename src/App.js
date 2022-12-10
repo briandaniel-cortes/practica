e33fc8721ccad4989091c1fd0ebad4ccf938a767
BrianProgramer <Briandanielprogramer@hotmail.com>
2022-12-10 03:11:52 -0600

    Subiendo practica / cuerpo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Header from './componentes/header';
+import Footer from './componentes/footer';
+function App() {
+  return (
+    <div>
+    <Header></Header>
+    <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
